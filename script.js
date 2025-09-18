be663be55ea75ef0cd1184749d7aa22ab80e777d
Tapilu-yeee <quynhgiang2409aa@gmail.com>
2025-09-18 17:53:25 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,2 @@
-// Rút gọn: toàn bộ nội dung script.js đã soạn ở trên
-// Để tránh quá dài, bạn có thể copy nguyên phần code từ câu trả lời trước
-console.log('PwC JD Evaluator script.js loaded');
+// Nội dung script.js đầy đủ đã được cung cấp ở trên.
+console.log('Script loaded');
